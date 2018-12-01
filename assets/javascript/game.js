c97cfc7d3729da980bfcd1b0d3b554fb15d9af99
Spatula Pete <petewx26@gmail.com>
2018-12-01 17:59:26 -0500

    add image files to assets folder. adjust javescript to link to new images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,81 @@
- 
-// declare global variables
-var targetNumber;
 var currentScore;
-var crystalHiddenValues=[];
-var imageSource=["blue-gem.png", "blue-gem.png", "blue-gem.png", "blue-gem.png"];
-var winCount = 0;
-var loseCount = 0;
-
-
-function addCrystalValue() {
-  var crystalValue = ($(this).attr("data-crystalvalue"));
-  crystalValue = parseInt(crystalValue);
-  currentScore += crystalValue;
-  console.log(currentScore);
-  if (currentScore === targetNumber) {
-    winCount++;
-    initGame();
-  }
-}
-
-function initGame() {
-  
-  currentScore = 0;
-  $("#crystals").empty();
-  
-  targetNumber = 19 + Math.floor((Math.random() * ((120-19) + 1)));
-  console.log(targetNumber);
-  $("#number-to-guess").text(targetNumber);
-  
-  for (var i=0; i < 4; i++) {
-    crystalHiddenValues[i] = Math.floor((Math.random() * 12) + 1);
-    console.log(crystalHiddenValues[i]);
-  };
-
-  // create four crystal buttons and assign a value to each
-  for (var i=0; i < imageSource.length; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "assets/images/" + imageSource[i]);
-    imageCrystal.attr("data-crystalvalue", crystalHiddenValues[i]);
-    $("#crystals").append(imageCrystal);
-  };
-   
-}
 
 window.onload = function() {
+    
+  crystalCollector.initGame();
+  
+};
 
-  initGame();
-  $(".crystal-image").click(addCrystalValue);
-  // create event listener for crystal-image class
-}
 
+var crystalCollector = {
+
+  targetNumber: 0,
+  crystalHiddenValues: [],
+  imageSource: ["pink-gem.png", "blue-gem.png", "green-gem.png", "purple-gem.png"],
+  winCount: 0,
+  loseCount: 0,
+
+
+  // add lines to print win & lose count to screen
+
+  addCrystalValue: function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    currentScore += crystalValue;
+    console.log(currentScore);
+    $("#current-total").text(currentScore);
+
+    if (currentScore === crystalCollector.targetNumber) {
+      crystalCollector.winCount++;
+      $("#count-wins").text(crystalCollector.winCount);
+      console.log("wins: " + crystalCollector.winCount);
+      $("#game-result").removeClass("alert alert-sucess alert-danger");
+      $("#game-result").addClass("alert alert-success");
+      $("#game-result").text("YOU WON!");
+      crystalCollector.initGame();
+      
+    }
+    else if (currentScore > crystalCollector.targetNumber) {
+      crystalCollector.loseCount++;
+      $("#count-losses").text(crystalCollector.loseCount);
+      console.log("losses: " + crystalCollector.loseCount);
+      $("#game-result").removeClass("alert alert-sucess alert-danger");
+      $("#game-result").addClass("alert alert-danger");
+      $("#game-result").text("YOU LOST!");
+      crystalCollector.initGame();
+      
+    }
+  },
+
+  initGame: function() {
+
+    currentScore = 0;
+    $("#crystals").empty();
+    $("#current-total").text(currentScore);
+    $("#count-wins").text(crystalCollector.winCount);
+    $("#count-losses").text(crystalCollector.loseCount);
+    
+    
+    crystalCollector.targetNumber = 19 + Math.floor((Math.random() * ((120-19) + 1)));
+    console.log(crystalCollector.targetNumber);
+    $("#number-to-guess").text(crystalCollector.targetNumber);
+
+    for (var i=0; i < 4; i++) {
+      crystalCollector.crystalHiddenValues[i] = Math.floor((Math.random() * 12) + 1);
+      console.log(crystalCollector.crystalHiddenValues[i]);
+    };
+
+  // create four crystal buttons and assign a value to each
+    for (var i=0; i < crystalCollector.imageSource.length; i++) {
+        var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "assets/images/" + crystalCollector.imageSource[i]);
+        imageCrystal.attr("data-crystalvalue", crystalCollector.crystalHiddenValues[i]);
+        $("#crystals").append(imageCrystal);
+    }; 
+
+    $(".crystal-image").click(crystalCollector.addCrystalValue);
+  }
+
+  
+};
